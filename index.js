76d20bb34a090500a7db4864d572c3566347d0bf
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2021-12-25 18:36:52 +0530

    added controller and router

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const port = 8000
 const app = express();
 
+// app using routes adding path.
+app.use('/',require('./routes'));
+
 
 app.listen(port,function(err){
     if(err){
-        console.log(`Error in running server on port : ${err.message}`);
+        console.log(`Error in running server on port : ${err}`);
+        return;
     }
  
     console.log(`Server running on port : ${port}`);
