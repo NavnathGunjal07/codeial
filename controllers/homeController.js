6568f5d9a926319fedef8205da16d50f36ef80f3
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-01-08 22:15:26 +0530

    displaying the created posts in homepage and added authrization to creating post form

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,25 @@
+const Post = require('../models/posts');
 module.exports.home = function(req, res){
-    if(!req.isAuthenticated()){
-        return res.redirect('/users/sign-in')
-    }
+    // Post.find({},function(err,posts){
+    //     return res.render('home.ejs',{
+    //         title: 'Codeial | Home',
+    //         header:true,
+    //         footer:true,
+    //         posts:posts
+    //     });
+    // });
+
+    //Populate the user of each posts
+    Post.find({}).populate('user').exec(function (err, posts) {
+    if (err){
+        console.log("Error in populating user homecontroller",err);
+        return;
+    } 
     return res.render('home.ejs',{
         title: 'Codeial | Home',
         header:true,
-        footer:true
+        footer:true,
+        posts:posts
     });
+  });
 }
