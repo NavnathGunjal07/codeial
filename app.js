0f48332241f4e8925e479b5c1ad3d6b5514b1444
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-01-13 01:20:26 +0530

    Added flash messages for login and logout using connect-flash

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware =require('node-sass-middleware');
-
-
+const flash = require('connect-flash');
+const customMiddleware = require('./config/middleware');
 
 
 app.use(sassMiddleware({
@@ -57,6 +57,8 @@ app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash());
+app.use(customMiddleware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
